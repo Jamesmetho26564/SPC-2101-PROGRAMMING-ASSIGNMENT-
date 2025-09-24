7db366bac8f36b3ec1862f2bcd752a2244ce7df8
Jamesmetho26564 <jamesmetho8@gmail.com>
2025-09-24 14:17:01 +0300

    Add files via upload

---
+++ Height,balance, phone.c
@@ -0,0 +1,28 @@
+// simple C program (Hello World) Height 
+/*
+Name: James 
+Description: James
+Registration No:CT101/G/26564/25 
+*/
+
+#include<stdio.h>;
+int main()
+{
+int height ;
+printf("enter your height(in meters)\n");
+scanf("%d",& height);
+printf (" your height is %d\n", height);
+
+int balance ;
+printf("enter your balance\n");
+scanf("%d",&  balance);
+printf("your balance is %d\n",   balance);
+
+int   number;
+printf(" enter your  number\n");
+scanf ("%d",& number);
+printf ("your phone is %d", number);
+}
+
+
+    
