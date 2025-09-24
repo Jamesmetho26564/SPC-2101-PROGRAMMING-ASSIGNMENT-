0c065be675eb15767268ea8d8f168a838eedc210
Jamesmetho26564 <jamesmetho8@gmail.com>
2025-09-24 14:16:25 +0300

    Add files via upload

---
+++ Semester 1 units.c
@@ -0,0 +1,24 @@
+// simple C program (Hello World)  Units for first Semester Computer science 
+/*
+Name: James Metho Kithuka 
+Description: James  Metho Kithuka 
+Registration No:CT101/G/26564/25
+Date:12/9/2025
+*/
+
+#include<stdio.h> printf()
+//main function 
+int main(){
+
+printf("COMPUTER SCIENCE FIRST SEMESTER UNITS 2025 FOR FIRST YEARS\n");
+
+printf("1. Fundamentals of ICT (ucu2105)\n");
+printf ("2. Introduction to computer architecture (spc2102)\n");
+printf("3. Communication skills (ucu2101)\n");
+printf("4. Analogue electronics (Sph2107)\n");
+printf("5. Discrete Mathematics (spm2100)\n");
+printf("6. Mathematics for Science (spm2104)\n");
+printf("7.  Physics (sph2101)\n");
+printf("8. Introduction to programming and problem solving (spc 2101)\n");
+}
+    
