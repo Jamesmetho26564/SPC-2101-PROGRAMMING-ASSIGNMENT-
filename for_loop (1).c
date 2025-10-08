ba9beeb40f528ab6ea5135ace574488788774991
Jamesmetho26564 <jamesmetho8@gmail.com>
2025-10-08 20:31:31 +0300

    Add files via upload

---
+++ for_loop (1).c
@@ -0,0 +1,28 @@
+// A program to demonstrate for loop 
+/*
+Name: METHO JAMES KITHUKA
+Registration No:CT101/G/26564/25
+Date:30/09/2025
+
+*/
+
+#include<stdio.h>;
+// main function
+int main()
+{
+
+//variable declaration
+int i;
+//start and stop 
+for(i=100;i<=100 && i>=50;i--)
+
+{printf("%d\t\n",i);
+}
+
+
+return 0;
+}
+
+
+    
+    
